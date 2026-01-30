9a40a53d694109bcdffb92c828b6cb56c3830f4e
kokirisari <168030365+kokirisari@users.noreply.github.com>
2026-01-30 13:00:03 +0000

    Updated MyGame.h

--- src/demo/MyGame.h
+++ src/demo/MyGame.h
@@ -2,31 +2,41 @@
 #define __TEST_GAME_H__
 
 #include "../engine/AbstractGame.h"
+#include <string>
 
 struct GameKey {
 	Point2 pos;
 	bool isAlive;
 };
 
+
 class MyGame : public AbstractGame {
-	private:
-		Rect box;
+private:
+	Rect box;
+	Vector2i velocity;
 
-		Vector2i velocity;
+	std::vector<std::shared_ptr<Button>> buttons;
+	std::vector<std::shared_ptr<GameKey>> gameKeys;
 
-		std::vector<std::shared_ptr<GameKey>> gameKeys;
+	MyEngineSystem engineSystem;
 
-		/* GAMEPLAY */
-		int score, numKeys, lives;
-		bool gameWon;
+	/* GAMEPLAY */
+	int score, numKeys, lives;
+	bool gameWon, buttonClicked;
 
-		void handleKeyEvents();
-		void update();
-		void render();
-		void renderUI();
-	public:
-        MyGame();
-		~MyGame();
+	void handleKeyEvents();
+	void update();
+	void render();
+	void renderUI();
+	void renderButtons();
+	void loadButtons();
+	void buttonsAction();
+public:
+	MyGame();
+	~MyGame();
+	SDL_Color primaryColor = { 255, 255, 255 };
+	SDL_Color secondaryColor = { 255, 255, 255 };
+	SDL_Color tertiaryColor = { 255, 255, 255 };
 };
 
 #endif
